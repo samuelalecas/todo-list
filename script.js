9d282a762b896e03adaedf669347f0c29adea73e
samuelalecas <samuelalecas@gmail.com>
2023-05-28 10:24:42 +0200

    Add Trigger Button Click on enter

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-let toDoList = [];
-const textBox = document.getElementById("textBox");
-
+let toDoList = {};
+let id = 0;
+const inputBox = document.getElementById("inputBox");
+const inputBoxBtn = document.getElementById("inputBoxBtn");
 
 const getValue = () => {
-    toDoList.push(textBox.value);
-    textBox.value = "";
+    newToDoItem();
+    inputBox.value = "";
     console.log(toDoList);
+}
+
+// Usar Enter para introducir datos
+// https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+
+inputBox.addEventListener("keypress", (event) => {
+    if(event.key === "Enter"){
+        // Evita el comportamiento por defecto de Enter
+        event.preventDefault();
+        // Llama al botón que se desea pulsar
+        inputBoxBtn.click();
+    }
+});
+
+const newToDoItem = () => {
+    toDoList[id] = {};
+    toDoList[id]["task"] = inputBox.value;
+    toDoList[id]["complete"] = false;
+    id++;
 }
