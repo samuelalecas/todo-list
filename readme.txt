0ad481c90f0945155ae3d44d5000b183e3e676ef
samuelalecas <samuelalecas@gmail.com>
2023-05-28 10:55:23 +0200

    Show list items when Send Button is pushed

--- readme.txt
+++ readme.txt
@@ -3,6 +3,7 @@ Pasos a seguir:
 1b. Cuando pulsas el botón, el texto del campo se almacena.
 2. El texto introducido ha de almacenarse en un array.
 2b. Se borra el campo al introducirse.
+2c. Hacer que al pulsar Enter haga lo mismo que pulsar Enviar.
 3. Cada vez que se introduce un texto, se inyecta un nuevo código HTML con el texto.
 5. Este código ha de tener un botón de editar, y un botón de borrar.
 6. Este código ha de tener un id único, que permita manejar los borrados y ediciones.
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let toDoList = {};
 let id = 0;
 const inputBox = document.getElementById("inputBox");
 const inputBoxBtn = document.getElementById("inputBoxBtn");
+const list = document.getElementById("list");
 
 const getValue = () => {
     newToDoItem();
@@ -25,5 +26,41 @@ const newToDoItem = () => {
     toDoList[id] = {};
     toDoList[id]["task"] = inputBox.value;
     toDoList[id]["complete"] = false;
+    insertNewToDoItem(id);
     id++;
+}
+
+const insertNewToDoItem = (id) => {
+    // Crea un li que tenga de id el número del objeto
+    let li = document.createElement("li");
+    li.setAttribute("id", id);
+    li.classList.add("toDoItem");
+
+    // Crea un checkbox con clase "checkbox"
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.classList.add("checkbox");
+
+    // Crea un div con clase "task"
+    let task = document.createElement("div");
+    task.setAttribute("id", `task-${id}`);
+    task.classList.add("task");
+    task.innerText = toDoList[id]["task"];
+
+    // Crea un botón con clase "editItemBtn"
+    let editBtn = document.createElement("button");
+    editBtn.classList.add("editBtn");
+    editBtn.innerText = "Editar";
+
+    // Crea un botón con clase "deleteItemBtn"
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("deleteBtn");
+    deleteBtn.innerText = "Borrar";
+
+    // Inserta cada elemento en el li
+    li.append(checkbox, task, editBtn, deleteBtn);
+
+    // Inserta el li dentro del ul
+    list.appendChild(li);
+
 }
